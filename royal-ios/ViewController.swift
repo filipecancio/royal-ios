c087c19aff9b54bce1e8f1db452bb2589184139b
Filipe Cancio <filipe.cancio@outlook.com>
2020-11-03 15:13:50 -0300

    feat: first version

---
+++ royal-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  royal-ios
+//
+//  Created by Felipe Soares Cancio on 03/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
